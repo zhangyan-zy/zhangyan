c88d2c0888eb103701dd65a07caec8bd20402f8d
CrazyHuskar <jsk19940118@163.com>
2020-03-05 18:34:18 +0800

    20200305

--- src/views/main-content.vue
+++ src/views/main-content.vue
@@ -28,7 +28,7 @@
             width="100%" height="100%" frameborder="0" scrolling="yes">
           </iframe>
           <keep-alive v-else>
-            <router-view v-if="item.name === mainTabsActiveName" />
+            <router-view v-if="item.name === mainTabsActiveName"/>
           </keep-alive>
         </el-card>
       </el-tab-pane>
@@ -36,43 +36,58 @@
     <!-- 主入口标签页 e -->
     <el-card v-else :body-style="siteContentViewHeight">
       <keep-alive>
-        <router-view />
+        <router-view/>
       </keep-alive>
     </el-card>
-    <p class="brand-info__intro" style="text-align: center">Copyright © 2016-2020 飞梦网络<a href="http://www.beian.miit.gov.cn">苏ICP备19010048号-1</a></p>
+    <p class="brand-info__intro" style="text-align: center">Copyright © 2016-2020 飞梦网络 <a
+      href="http://www.beian.miit.gov.cn">苏ICP备19010048号-1</a></p>
   </main>
 </template>
 
 <script>
-  import { isURL } from '@/utils/validate'
+  import {isURL} from '@/utils/validate'
+
   export default {
     data () {
-      return {
-      }
+      return {}
     },
     computed: {
       documentClientHeight: {
-        get () { return this.$store.state.common.documentClientHeight }
+        get () {
+          return this.$store.state.common.documentClientHeight
+        }
       },
       menuActiveName: {
-        get () { return this.$store.state.common.menuActiveName },
-        set (val) { this.$store.commit('common/updateMenuActiveName', val) }
+        get () {
+          return this.$store.state.common.menuActiveName
+        },
+        set (val) {
+          this.$store.commit('common/updateMenuActiveName', val)
+        }
       },
       mainTabs: {
-        get () { return this.$store.state.common.mainTabs },
-        set (val) { this.$store.commit('common/updateMainTabs', val) }
+        get () {
+          return this.$store.state.common.mainTabs
+        },
+        set (val) {
+          this.$store.commit('common/updateMainTabs', val)
+        }
       },
       mainTabsActiveName: {
-        get () { return this.$store.state.common.mainTabsActiveName },
-        set (val) { this.$store.commit('common/updateMainTabsActiveName', val) }
+        get () {
+          return this.$store.state.common.mainTabsActiveName
+        },
+        set (val) {
+          this.$store.commit('common/updateMainTabsActiveName', val)
+        }
       },
       siteContentViewHeight () {
         var height = this.documentClientHeight - 50 - 30 - 2 - 30
         if (this.$route.meta.isTab) {
           height -= 40
-          return isURL(this.$route.meta.iframeUrl) ? { height: height + 'px' } : { minHeight: height + 'px' }
+          return isURL(this.$route.meta.iframeUrl) ? {height: height + 'px'} : {minHeight: height + 'px'}
         }
-        return { minHeight: height + 'px' }
+        return {minHeight: height + 'px'}
       }
     },
     methods: {
@@ -80,7 +95,7 @@
       selectedTabHandle (tab) {
         tab = this.mainTabs.filter(item => item.name === tab.name)
         if (tab.length >= 1) {
-          this.$router.push({ name: tab[0].name })
+          this.$router.push({name: tab[0].name})
         }
       },
       // tabs, 删除tab
@@ -89,13 +104,13 @@
         if (this.mainTabs.length >= 1) {
           // 当前选中tab被删除
           if (tabName === this.mainTabsActiveName) {
-            this.$router.push({ name: this.mainTabs[this.mainTabs.length - 1].name }, () => {
+            this.$router.push({name: this.mainTabs[this.mainTabs.length - 1].name}, () => {
               this.mainTabsActiveName = this.$route.name
             })
           }
         } else {
           this.menuActiveName = ''
-          this.$router.push({ name: 'home' })
+          this.$router.push({name: 'home'})
         }
       },
       // tabs, 关闭当前
@@ -110,14 +125,14 @@
       tabsCloseAllHandle () {
         this.mainTabs = []
         this.menuActiveName = ''
-        this.$router.push({ name: 'home' })
+        this.$router.push({name: 'home'})
       },
       // tabs, 刷新当前
       tabsRefreshCurrentHandle () {
         var tempTabName = this.mainTabsActiveName
         this.removeTabHandle(tempTabName)
         this.$nextTick(() => {
-          this.$router.push({ name: tempTabName })
+          this.$router.push({name: tempTabName})
         })
       }
     }
