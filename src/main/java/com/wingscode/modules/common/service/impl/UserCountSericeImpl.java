cd221ba145403ebd1a960664cd9abdae3470a3b1
CrazyHuskar <jsk19940118@163.com>
2020-03-12 18:19:40 +0800

    20200312

--- src/main/java/com/wingscode/modules/common/service/impl/UserCountSericeImpl.java
+++ src/main/java/com/wingscode/modules/common/service/impl/UserCountSericeImpl.java
@@ -3,13 +3,11 @@ package com.wingscode.modules.common.service.impl;
 import com.wingscode.modules.common.dao.LeadsDao;
 import com.wingscode.modules.common.dao.UserTestDao;
 import com.wingscode.modules.common.service.UserCountService;
+
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -30,26 +28,21 @@ public class UserCountSericeImpl implements UserCountService {
     @Override
     public List leadersList(Map<String, Object> params) {
 
-         String par= (String) params.get("parentId");
-         Integer parentId =null;
-         if(StringUtils.isNotEmpty(par) ){
-             parentId=Integer.parseInt(par);
-         }
-
+        String par = (String) params.get("parentId");
+        Integer parentId = null;
+        if (StringUtils.isNotEmpty(par)) {
+            parentId = Integer.parseInt(par);
+        }
         String date1 = (String) params.get("date1");
         String date2 = (String) params.get("date2");
-
-
-        System.out.println(parentId+"_________");
-        List list = userTestDao.queryUserList(parentId,date1,date2);
-        System.out.println(list+"12321312");
+        List list = userTestDao.queryUserList(parentId, date1, date2);
         return list;
     }
 
     @Override
     public List orderAgentList(Map<String, Object> params) {
         List list = userTestDao.OrderAgentList(params);
-        return  list;
+        return list;
     }
     //
     // @Override
