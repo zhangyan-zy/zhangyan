9c94a50746f77236caa858b87c69cccad9f7acfc
zhangyan <1911498970@qq.com>
2020-03-30 17:23:00 +0800

    zy-文件导出

--- src/main/java/com/wingscode/util/ExcelOutUtil.java
+++ src/main/java/com/wingscode/util/ExcelOutUtil.java
@@ -1,27 +1,29 @@
 package com.wingscode.util;
 
-
 import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
 
 /**
  * Author：张延
- * Date：2020-03-28 11:11
+ * Date：2020-03-30 15:22
  * Description：<描述>
  */
-public class ExcelUtil {
+public class ExcelOutUtil {
+
 
     /**
-     * 导出Excel
+     * 将数据导出成Excel文件
+     *
      * @param sheetName sheet名称
      * @param title 标题
      * @param values 内容
      * @param wb HSSFWorkbook对象
      * @return
      */
-    public static HSSFWorkbook getHSSFWorkbook(String sheetName, String []title, String [][]values, HSSFWorkbook wb){
+    public static HSSFWorkbook getHSSFWorkbook(String sheetName, String[] title, String[][] values, HSSFWorkbook wb) {
 
         // 第一步，创建一个HSSFWorkbook，对应一个Excel文件
-        if(wb == null){
+        if (wb == null) {
             wb = new HSSFWorkbook();
         }
 
@@ -33,23 +35,29 @@ public class ExcelUtil {
 
         // 第四步，创建单元格，并设置值表头 设置表头居中
         HSSFCellStyle style = wb.createCellStyle();
-    //    style.setAlignment(HSSFCellStyle.ALIGN_CENTER); // 创建一个居中格式
-
-        //声明列对象
+        style.setAlignment(HorizontalAlignment.CENTER); // 创建一个居中格式
+        //设置单元格宽度自适应，在此基础上把宽度调至1.5倍
+        for (int i = 0; i < title.length; i++) {
+            sheet.autoSizeColumn(i, true);
+            sheet.setColumnWidth(i, sheet.getColumnWidth(i) * 15 / 10);
+        }
+        // 声明列对象
         HSSFCell cell = null;
 
-        //创建标题
-        for(int i=0;i<title.length;i++){
+        // 创建标题
+        for (int i = 0; i < title.length; i++) {
             cell = row.createCell(i);
             cell.setCellValue(title[i]);
             cell.setCellStyle(style);
+            sheet.autoSizeColumn(i);
+            sheet.setColumnWidth(i, sheet.getColumnWidth(i) * 17 / 10);
         }
 
-        //创建内容
-        for(int i=0;i<values.length;i++){
+        // 创建内容
+        for (int i = 0; i < values.length; i++) {
             row = sheet.createRow(i + 1);
-            for(int j=0;j<values[i].length;j++){
-                //将内容按顺序赋给对应的列对象
+            for (int j = 0; j < values[i].length; j++) {
+                // 将内容按顺序赋给对应的列对象
                 row.createCell(j).setCellValue(values[i][j]);
             }
         }
